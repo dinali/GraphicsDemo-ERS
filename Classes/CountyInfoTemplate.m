93da6002d182134b01608f270a3ed557c3abef7c
Dina Li <dinali@ers.usda.gov>
2013-04-12 16:34:06 -0400

    version 1 Populate Callout
    sample adding callout

---
+++ Classes/CountyInfoTemplate.m
@@ -0,0 +1,34 @@
+// Copyright 2012 ESRI
+//
+// All rights reserved under the copyright laws of the United States
+// and applicable international laws, treaties, and conventions.
+//
+// You may freely redistribute and use this sample code, with or
+// without modification, provided you include the original copyright
+// notice and use restrictions.
+//
+// See the use restrictions at http://help.arcgis.com/en/sdk/10.0/usageRestrictions.htm
+//
+
+#import "CountyInfoTemplate.h"
+
+
+@implementation CountyInfoTemplate
+
+// text for the graphic will be the name attribute of the feature
+- (NSString *)titleForGraphic:(AGSGraphic *)graphic screenPoint:(CGPoint)screen mapPoint:(AGSPoint *)map {
+    
+   // return [graphic attributeAsStringForKey:@"NAME"];
+    return [graphic attributeAsStringForKey:@"St_Name"];
+
+}
+
+//details for the callout
+- (NSString *)detailForGraphic:(AGSGraphic *)graphic screenPoint:(CGPoint)screen mapPoint:(AGSPoint *)map {
+
+    return [NSString stringWithFormat:@"'90: %@, '99: %@", [graphic attributeAsStringForKey:@"POP2010"], [graphic attributeAsStringForKey:@"POP2010"]];
+
+}
+
+
+@end
